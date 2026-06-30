cc9c22d2c9d95f7421395bf1f7a9e2b55fe033ed
Jayant <saigee2703@gmail.com>
2026-06-30 22:22:17 +0530

    Implement basic user API foundation with FastAPI and Pydantic

--- main.py
+++ main.py
@@ -1,8 +1,35 @@
 from fastapi import FastAPI
-app=FastAPI()
+from pydantic import BaseModel
+
+app = FastAPI()
+
+users = []
+
+class UserCreate(BaseModel):
+    name: str
+    email: str
+
 @app.get("/")
 def home():
     return {
-    "message":"Banking Backend is Alive"
-}
+        "message": "Banking Backend is Alive"
+    }
 
+@app.get("/users")
+def get_users():
+    return users
+
+@app.post("/users")
+def create_user(user: UserCreate):
+    new_user = {
+        "id": len(users) + 1,
+        "name": user.name,
+        "email": user.email
+    }
+
+    users.append(new_user)
+
+    return {
+        "message": "User created successfully",
+        "user": new_user
+    }
